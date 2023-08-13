83b5efd35b827900e7e920a3d35c2a54e4774614
OneClickPablo <92474595+OneClickPablo123@users.noreply.github.com>
2023-08-13 17:19:58 +0200

    Update ItemPickUp.cs

--- Assets/Scripts/ItemPickUp.cs
+++ Assets/Scripts/ItemPickUp.cs
@@ -1,1 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class ItemPickUp : MonoBehaviour
+{
+    public bool hasItem; 
+    GameObject gamemanager;
+GameManager managerScript;
+
+    public void Start()
+{
+    gamemanager = GameObject.Find("gamemanager"); 
+    managerScript = gamemanager.GetComponent<ManagerScript>();
+}
+    
+    public void Update(){}
+
+        public void OnTriggerEnter2D(Collision collision)
+
+    {
+                                if(collision.gameObject.CompareTag("Player")
+
+{
+    if(!hasItem && Input.GetKeyDown(KeyCode.F))
+
+    {
+    hasItem = true;
+    
+    }
+}
+
+    }
+}

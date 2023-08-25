726ae84c2ca063d4d24d5b91717339da431889ab
OneClickPablo <92474595+OneClickPablo123@users.noreply.github.com>
2023-08-25 12:54:27 +0200

    Create Surface.cs

---
+++ Assets/Scripts/Surface.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using UnityEngine;
+
+public class Surface : MonoBehaviour
+{
+  public bool isWood;
+  public bool isStone;
+  public bool isGras;
+  public bool isSnow;
+
+}
